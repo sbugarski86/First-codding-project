b953a7cd70346a6088a370a7ceb249830c86af1d
Stevan Bugarski <bugarski.stevan@gmail.com>
2019-09-17 17:47:04 -0400

    COMMIT

---
+++ js/script.js
@@ -0,0 +1,12 @@
+	/*For the sticky navigation */
+$(document).ready(function() {
+	$('.js--section-features').waypoint(function(direction) {
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+  offset: '65%'
+});
+});
