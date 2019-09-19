2af3813fd8c279f70407fe79c13f532b33489f55
Stevan Bugarski <bugarski.stevan@gmail.com>
2019-09-19 11:38:51 -0400

    ommit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 	/*For the sticky navigation */
-$(document).ready(function() {
-	$('.js--section-features').waypoint(function(direction) {
-		if (direction == "down") {
-			$('nav').addClass('sticky');
-		} else {
-			$('nav').removeClass('sticky');
-		}
-	}, {
-  offset: '80p%'
-});
-});
-
-$('.carousel').carousel({
-	interval: 3000,
-	keyboard: true,
-	pause: 'hover',
-	wrap: true
-});
-
-$('#slider4').on('slide.bs.carousel', function () {
-	console.log('SLIDE!');
-});
-
-$('#slider4').on('slid.bs.carousel', function () {
-	console.log('SLID!');
-});
+	$(document).ready(function() {
+		$('.js--section-features').waypoint(function(direction) {
+			if (direction == "down") {
+				$('nav').addClass('sticky');
+			} else {
+				$('nav').removeClass('sticky');
+			}
+		}, {
+		offset: '80p%'
+	});
+	});
+	
+	$('.carousel').carousel({
+		interval: 3000,
+		keyboard: true,
+		pause: 'hover',
+		wrap: true
+	});
+	
+	$('#slider4').on('slide.bs.carousel', function () {
+		console.log('SLIDE!');
+	});
+	
+	$('#slider4').on('slid.bs.carousel', function () {
+		console.log('SLID!');
+	});
 
